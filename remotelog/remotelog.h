735ca43ea778522e81d2441460be5cd5877c36ee
Jiri Bruchanov <jbruchanov@gmail.com>
2013-07-20 17:02:58 +0100

    Initial Commit

---
+++ remotelog/remotelog.h
@@ -0,0 +1,13 @@
+//
+//  remotelog.h
+//  remotelog
+//
+//  Created by Joe Scurab on 7/20/13.
+//  Copyright (c) 2013 Jiri Bruchanov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface remotelog : NSObject
+
+@end
---
+++ remotelog/remotelog.m
@@ -0,0 +1,13 @@
+//
+//  remotelog.m
+//  remotelog
+//
+//  Created by Joe Scurab on 7/20/13.
+//  Copyright (c) 2013 Jiri Bruchanov. All rights reserved.
+//
+
+#import "remotelog.h"
+
+@implementation remotelog
+
+@end
