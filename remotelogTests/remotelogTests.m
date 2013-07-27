7efa38b9cd884e08326e941b220976cddf6ee107
Jiri Bruchanov <jbruchanov@gmail.com>
2013-07-27 17:23:17 +0100

    Initial Commit

---
+++ remotelogTests/remotelogTests.m
@@ -0,0 +1,32 @@
+//
+//  remotelogTests.m
+//  remotelogTests
+//
+//  Created by Joe Scurab on 7/27/13.
+//  Copyright (c) 2013 Jiri Bruchanov. All rights reserved.
+//
+
+#import "remotelogTests.h"
+
+@implementation remotelogTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in remotelogTests");
+}
+
+@end
